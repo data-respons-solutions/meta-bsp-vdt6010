1044076de6ce3e5af0d8ce3528483d44784900bb
Mikko Salomäki <ms@datarespons.se>
2020-10-13 14:39:41 +0200

    Add image_install 0.1

---
+++ recipes-bsp/image-install/files/image-install.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+die() {
+	echo $1
+	exit 1
+}
+
+if [ "$#" -lt "1" ]; then
+	die "Usage: $0 IMAGE"
+fi
+
+echo "Image details:"
+echo "  name: ${1}"
+
+# Clear and create partitions
+# Rootfs partition labels "rootfs1" and "rootfs2" are critical
+# if using common root-swap utility
+blockdev-init /dev/mmcblk1 \
+	rootfs1 ext4 1000 \
+	rootfs2 ext4 1000 \
+	app ext4 500 \
+	data ext4 500 \
+		|| die "Failed initializing blockdev"
+
+
+# Install image
+mkdir -v /mnt/rootfs1 || die "rootfs1 mountpoint already exists"
+mount -v -L rootfs1 /mnt/rootfs1 || die "failed mounting rootfs1"
+echo "Extracting rootfs.."
+tar -xf ${1} -C /mnt/rootfs1 || die "Failed extracting rootfs"
+umount -v /mnt/rootfs1 || die "Failed unmounting rootfs1"
+rmdir -v /mnt/rootfs1 || die "Failed removing mountpoint"
+
+## set nvram variables
+NVRAM_SYSTEM_UNLOCK=16440 nvram --sys set SYS_BOOT_PART 1
+NVRAM_SYSTEM_UNLOCK=16440 nvram --sys set SYS_BOOT_SWAP 1
+NVRAM_SYSTEM_UNLOCK=16440 nvram --sys set SYS_BOOT_VERIFIED true
+
+# Sync filesystem
+sync || die "Failed syncing filesystem"
+
+# Image installation successfull
+echo "Image installation completed"
+exit 0
---
+++ recipes-bsp/image-install/image-install_0.1.bb
@@ -0,0 +1,14 @@
+DESCRIPTION = "vdt6010 image installer for fresh installation from USB"
+LICENSE = "MIT"
+LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
+
+SRC_URI += " \
+	file://image-install.sh \
+"
+
+RDEPENDS_${PN} += "blockdev-init nvram"
+
+do_install () {
+    install -d ${D}${sbindir}
+    install -m 0755 ${WORKDIR}/image-install.sh ${D}${sbindir}/image-install
+}
